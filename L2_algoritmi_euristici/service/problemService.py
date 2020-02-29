6a51dc331a88fc4a93e0c791558d24be8a50ecfc
Ciovnicu Narcis <48652642+Narcissss@users.noreply.github.com>
2020-02-29 14:53:36 +0200

    Changed the algorithm for the problem

--- L2_algoritmi_euristici/service/problemService.py
+++ L2_algoritmi_euristici/service/problemService.py
@@ -5,31 +5,41 @@ from domain.problem import ProblemResult, Problem
 class ProblemService:
 
     def __init__(self):
-        self._startCity = 0
+        pass
 
     """
     Complexity: O(n*n) - time, O(n) - additional space
         n - number of cities
     """
     def findShothestPath_Greedy(self, problem: Problem) -> ProblemResult:
-        visited = [False] * problem.numberOfCities
         totalTime = 0
-        currentCity = self._startCity
-        visited[currentCity] = True
-        visitedCities = 1
-        cities = [currentCity] # start city
+        startCity = 0
+        clientCity = problem.numberOfCities - 1
+        cities = []
+        cities.append(startCity)
+
+        totalTime += self._calculateShortestPath(startCity, clientCity, problem.distances, cities, problem.numberOfCities)
+        totalTime += self._calculateShortestPath(clientCity, startCity, problem.distances, cities, problem.numberOfCities)
         
-        while visitedCities < problem.numberOfCities:
-            shorthestDistanceToNextCity = self._findShorthestDistanceToNextCity(problem.distances[currentCity], visited)
+        return ProblemResult(problem.numberOfCities, cities, totalTime)
+
+    def _calculateShortestPath(self, startCity: int, endCity: int, distances: List[List[int]], cities: List[int], cityCount: int) -> int:
+        visited = [False] * cityCount
+        totalTime = 0
+        currentCity = startCity
+        visited[startCity] = True
+        
+        while currentCity != endCity:
+            shorthestDistanceToNextCity = self._findShorthestDistanceToNextCity(distances[currentCity], visited)
             currentCity = shorthestDistanceToNextCity["city"]
             totalTime += shorthestDistanceToNextCity["distance"]
             cities.append(currentCity)
             visited[currentCity] = True
-            visitedCities += 1
-        totalTime += problem.distances[currentCity][0]
+        return totalTime
 
-        return ProblemResult(problem.numberOfCities, cities, totalTime)
-
+    """
+    Evaluation function - f
+    """
     def _findShorthestDistanceToNextCity(self, distances: List[int], visited: List[bool]) -> Dict[str, int]:
         minDistance = 999 * 999
         city = 0
